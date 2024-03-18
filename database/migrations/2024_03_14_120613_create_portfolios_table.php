36826d34b2f67dc1936204297332e4096cc2c1ac
hadeel <hadeel.alawar.03@gmail.com>
2024-03-18 18:48:11 +0300

    remove lines

--- database/migrations/2024_03_14_120613_create_portfolios_table.php
+++ database/migrations/2024_03_14_120613_create_portfolios_table.php
@@ -15,9 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string("url");
             $table->foreignId("job_seeker_id")->nullable()->references("id")->on("job_seekers");
-
-
-
             $table->foreignId("company_id")->nullable()->references("id")->on("companies");
             $table->timestamps();
         });
