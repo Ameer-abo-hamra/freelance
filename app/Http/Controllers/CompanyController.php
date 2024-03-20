aa0e25dc8d7affa6c361e7e802c18138f032dc88
hadeel <hadeel.alawar.03@gmail.com>
2024-03-20 16:57:41 +0300

    register/login

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use App\Traits\Response;
 use Validator;
+use Auth;
 
 use Illuminate\Http\Request;
 
@@ -15,11 +17,35 @@ class CompanyController extends Controller
         $validator = validator::make($request->all(), [
             "name" => "required|unique:companies| max:15",
             "employee_number" => "required |integer | min:10 | max:500000",
-
         ]);
 
         if ($validator->fails()) {
             return $this->returnError($validator->errors()->first());
         }
+        Company::create([
+            "name" => $request->name,
+            "establishment_date" => $request->establishment_date,
+            "employee_number" => $request->employee_number
+        ]);
+        return $this->returnSuccess("your account created successfully");
+    }
+
+    public function login(Request $request){
+        $validator = validator::make($request->all(), [
+            "name" => "required|unique:companies| max:15",
+            "employee_number" => "required |integer | min:10 | max:500000",
+        ]);
+
+        if ($validator->fails()) {
+            return $this->returnError($validator->errors()->first());
+        }
+        $credential=$request->only("name");
+        $token=Auth::guard("web-company")->attempt($credential);
+        if($token){
+            $company=Auth::guard("web-company")->user();
+            $company->api=$token;
+            return $this->returnData("U R logged-in successfully","company data",$company);
+        }
+        return $this->returnError("your data is invalid .. enter it again");
     }
 }
